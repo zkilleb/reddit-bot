39f81a1e10643966072db120578e2f53274cfc9f
Zach Killebrew <zkilleb@siue.edu>
2020-03-15 16:59:01 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Reddit Bot
+
+## Installation
+
+Run npm install
+Create creds.js at top level of project with format
+```javascript
+module.exports = {
+    creds: {
+        "clientId": <clientId>,
+        "clientSecret": <clientSecret>,
+        "userAgent": <userAgent>,
+        "username": <userName>,
+        "password": <password>
+    },
+    subreddit: <subreddit_name>,
+    botOwner: <botowner_username>,
+    botName: <botname>,
+    flair: <flair_text>,
+    postTitle: <post_text>
+};
+```
+Run npm start
---
+++ app.js
@@ -0,0 +1,39 @@
+'use strict';
+const snoowrap = require('snoowrap');
+const schedule = require('node-schedule');
+const { creds, subreddit, botOwner, botName, flair, postTitle } = require('./creds')
+
+const r = new snoowrap(creds);
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+function main() {
+    //Post monthly stickied post
+    schedule.scheduleJob('* 0 1 * *', () => {
+        let today = new Date();
+        r.getSubreddit(subreddit).submitSelfpost({ title: `${postTitle} - ${months[today.getMonth()]} ${today.getFullYear()}`, text: '' })
+            .sticky({ num: 1 }).distinguish().approve().assignFlair({ text: flair })
+            .reply(`This thread was automatically posted by /u/${botName}. For questions of problems, contact /u/${botOwner}.`);
+        console.log(`Monthly post posted at ${today} to ${subreddit}`);
+    });
+
+    //Send modqueue to specified user at end of day
+    schedule.scheduleJob('0 0 * * *', () => {
+        let today = new Date();
+        let modQueue = [];
+        r.getSubreddit(subreddit).getModqueue({ limit: 100 }).forEach(item => {
+            modQueue.push({ title: item.link_title, reason: item.user_reports, author: item.author, body: item.body });
+            let tempString = '';
+            modQueue.forEach(report => {
+                tempString += JSON.stringify(report);
+            });
+            r.composeMessage({
+                to: botOwner,
+                subject: `Modqueue ${today}`,
+                text: tempString
+            });
+            console.log(`Modqueue message sent at ${today} to ${botOwner}`);
+        });
+    });
+}
+
+main();
